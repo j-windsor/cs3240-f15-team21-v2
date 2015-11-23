b3536fff1abcb87a2336b6e7811c609e7523b885
j-windsor <jayhwindsor@gmail.com>
2015-11-22 21:38:43 -0500

    Final Heroku Config

--- SecureShare/settings.py
+++ SecureShare/settings.py
@@ -94,10 +94,17 @@ WSGI_APPLICATION = 'SecureShare.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+"""
 import dj_database_url
 DATABASES = {
     'default': dj_database_url.config()
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Enable Persistent Connections
