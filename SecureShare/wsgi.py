402d2e7d4b237102a29697053694899b63c5e8ac
j-windsor <jayhwindsor@gmail.com>
2015-11-22 20:40:16 -0500

    WSGI change

--- SecureShare/wsgi.py
+++ SecureShare/wsgi.py
@@ -11,10 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SecureShare.settings")
 
