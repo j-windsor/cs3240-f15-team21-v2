3103c7bf5d2a9ce71c00a7258ce94fc0a1136b32
j-windsor <jayhwindsor@gmail.com>
2015-11-22 20:01:59 -0500

    Heroku WSGI changes

--- SecureShare/wsgi.py
+++ SecureShare/wsgi.py
@@ -11,9 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SecureShare.settings")
+
+application = get_wsgi_application()
